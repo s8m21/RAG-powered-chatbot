0299afc0fe2686d7e79f99bfd6894d51763aba9a
s8m21 <maaheshwari.s@northeastern.edu>
2024-06-13 15:58:48 -0400

    "commit"

--- ingestion.py
+++ ingestion.py
@@ -10,15 +10,15 @@ load_dotenv()
 if __name__ == "__main__":
     print("ingesting data...")
     
-    # load pdf document
+    # loading pdf document
     loader = PyPDFLoader("data\policy-booklet-0923.pdf")
     document = loader.load()
     
-    # split entire documents into chunks  
+    # splitting entire documents into chunks  
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     texts = text_splitter.split_documents(document)
     print(f"created {len(texts)} chunks")
 
-    # create vector embeddings and save it in pinecone database
+    # creating vector embeddings and saving it in pinecone database
     embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
     PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
